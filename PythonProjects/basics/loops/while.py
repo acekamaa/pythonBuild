41affde8742cfa5f907e94b857a7efbb92564fc8
- <kvnkamaa@gmail.com>
2024-10-31 12:25:06 +0300

    while loop

---
+++ PythonProjects/basics/loops/while.py
@@ -0,0 +1,14 @@
+i = 1
+while i < 6:
+    print(i)
+    #exit the loop when i = 3
+    #if i ==3:
+        #break
+    
+    #continue keyword
+    #if i == 3:
+    # continue
+    #print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
