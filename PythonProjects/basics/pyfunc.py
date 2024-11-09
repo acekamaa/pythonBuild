cd881bd6ab8e60fd2dd051a5b9854a47679a7e37
- <kvnkamaa@gmail.com>
2024-11-09 16:12:35 +0300

    python functions and arguments

---
+++ PythonProjects/basics/pyfunc.py
@@ -0,0 +1,14 @@
+#functions definitions start with keyword 'def'
+def my_func():
+    print("Hello, passing greetings from my_func")
+
+#calling my_func
+my_func()
+
+#my_function args
+def my_function(fname):
+    print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
