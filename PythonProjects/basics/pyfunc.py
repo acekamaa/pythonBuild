0403b7324f354b6d993797cc15bf129946e9c46c
- <kvnkamaa@gmail.com>
2024-11-09 16:23:10 +0300

    key-value arguments

--- PythonProjects/basics/pyfunc.py
+++ PythonProjects/basics/pyfunc.py
@@ -12,3 +12,15 @@ def my_function(fname):
 my_function("Emil")
 my_function("Tobias")
 my_function("Linus")
+
+#arbitrary arguments - *args
+def my_funci(*kids):
+    print("The youngest child is " + kids[2])
+
+my_funci("Emil", "Tobias", "Linus")
+
+#sending arguments with key = value syntax
+def key_func(child3, child2, child1):
+    print("The youngest child is " + child3)
+
+key_func(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
